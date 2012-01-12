e256faabf3f85ee7a77e0fa1f6654fb16f4fedc6
Eric Wustrow <ewust@umich.edu>
2012-01-11 22:42:33 -0500

    adding mangle/unmangle python poc

---
+++ mangletest.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+# iptables -N planb
+# iptables -A planb -d 5.5.5.5 -j NFQUEUE --queue-num 1
+# iptables -A OUTPUT -j planb
+
+from netfilterqueue import NetfilterQueue
+import socket
+import dpkt
+import dnet
+import time
+
+RELAY = socket.inet_aton('68.40.51.184')
+PROXY = socket.inet_aton('141.212.109.239')
+MTU = 500
+
+out = dnet.ip()
+
+def print_and_accept(pkt):
+    bytes = pkt.get_payload()
+    print pkt, len(bytes)
+
+    inner_ip_hdr = dpkt.ip.IP(bytes) # us -> proxy
+                                     # (outer will be "proxy" -> relay)
+    payload_bytes = str(inner_ip_hdr.data)
+    
+    # IP ( UDP ( real_IP(...) ) ) 
+    # if fragments, also send
+    # IP ( UDP ( real_IP_frag1(...) ) ) 
+    # IP ( UDP ( real_IP_frag2(...) ) ) .. etc
+    for pos in range(0,len(payload_bytes), MTU):
+        
+        # make an inner ip fragment
+        frag = payload_bytes[pos:pos+MTU]
+        cur_inner_ip_hdr = dpkt.ip.IP(str(inner_ip_hdr))
+        cur_inner_ip_hdr.sum = 0
+        cur_inner_ip_hdr.data = frag
+        cur_inner_ip_hdr.off = pos/8
+        if pos+len(frag) < len(payload_bytes):
+            cur_inner_ip_hdr.off |= dpkt.ip.IP_MF
+
+        udp = dpkt.udp.UDP(sport=1111, dport=2222, data=str(cur_inner_ip_hdr))
+        udp.ulen += len(udp.data)
+        p = dpkt.ip.IP(src=PROXY, dst=RELAY, p=0x11, data=udp)
+        p.len += len(p.data)
+        pkt_out = str(p)
+        print "sending %d bytes..." % len(pkt_out)
+        if (pos != 0):
+            time.sleep(0.001)
+        print out.send(pkt_out)
+
+    pkt.drop()
+
+nfqueue = NetfilterQueue()
+nfqueue.bind(1, print_and_accept)
+try:
+    nfqueue.run()
+except KeyboardInterrupt:
+    print
---
+++ unmangletest.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+# iptables -N pb_in
+# iptables -A pb_in -p icmp -j NFQUEUE --queue-num 2
+# iptables -A INPUT -j pb_in
+
+from netfilterqueue import NetfilterQueue
+import socket
+import dpkt
+import dnet
+
+out = dnet.ip()
+
+class PacketError(Exception):
+    pass
+
+def unnest_payload(bytes):
+    ip = dpkt.ip.IP(bytes)
+    if not isinstance(ip.data, dpkt.icmp.ICMP):
+        raise PacketError
+    icmp = ip.data
+    if not isinstance(icmp.data, dpkt.icmp.ICMP.Unreach):
+        raise PacketError
+    unreach = icmp.data
+    if not isinstance(unreach.data, dpkt.ip.IP):
+        raise PacketError
+    ip2 = unreach.data
+    if not isinstance(ip2.data, dpkt.udp.UDP):
+        raise PacketError
+    udp = ip2.data
+    ip3 = dpkt.ip.IP(str(udp.data))
+    return ip3
+
+def handle_packet(pkt):
+    print pkt
+    data = pkt.get_payload()
+    print data.encode('hex')
+    try:
+        ip = unnest_payload(data)
+        print str(ip).encode('hex')
+        out.send(str(ip))
+        print 'drop'
+        pkt.drop()
+    except PacketError:
+        print 'accept'
+        pkt.accept()
+
+nfqueue = NetfilterQueue()
+nfqueue.bind(2, handle_packet)
+try:
+    nfqueue.run()
+except KeyboardInterrupt:
+    print
