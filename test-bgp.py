fd74d100d5e8390a8174579a52e25e64121f5e68
Eric Wustrow <ewust@umich.edu>
2012-01-13 16:11:52 -0500

    modularize test-bgp, and read until we have 5-ish good hops, and return their true MTUs

--- test-bgp.py
+++ test-bgp.py
@@ -6,32 +6,52 @@ import random
 import socket
 import struct
 
-# TODO: don't read through a 385k line file only to pull out 100 random lines
-hosts = [] 
-# For each network, pick a random host in it
-for line in sys.stdin.readlines():
+'''
+Input: "1.2.3.0/24"
+Output: "1.2.3.87"
+'''
+def get_random_host_in_network(network):
     # parse 1.2.3.0/24 into prefix (0x01020304) and netmask (0xffffff00)
-    prefix_str, prefix_len = line.split('/')
+    prefix_str, prefix_len = network.split('/')
     prefix, = struct.unpack('!I', socket.inet_aton(prefix_str))
     netmask = ~(2**(32 - int(prefix_len)) - 1)
 
     # pick a random host
-    rand_host = socket.inet_ntoa(struct.pack('!I', 
+    return socket.inet_ntoa(struct.pack('!I', 
                     prefix | (~netmask & random.randint(0, 0xffffffff))))
 
-    # add it to our list
-    hosts.append(rand_host)
-
-    #print '%s -> %s' % (line[0:-1], socket.inet_ntoa(rand_host))
+def get_hops(prefixes_file=sys.stdin):
+    hosts = [] 
+    # For each network, pick a random host in it
+    lines = prefixes_file.readlines()
+    best_hops = {}
+    while len(best_hops.keys()) < 5:
     
+        index = random.randint(0, len(lines))
+         
+        rand_host = get_random_host_in_network(lines[index])
+     
+        hops = testttl.test_dest(rand_host) 
     
-
-# pick 100 hosts at random, and traceroute them to get their payload lengths
-for i in range(0, 100):
-    index = random.randint(0, len(hosts)) 
-
-    print '-----'
-    print '%s' % hosts[index]
-    hops = testttl.test_dest(hosts[index])
+        #print '-----'
+        #print '%s' % rand_host
+        #testttl.print_hops(hops)
+        
+        payload_len_hop, payload_len = testttl.get_max_payload_len(hops)
+        if (payload_len >= 64):
+            best_hops[(rand_host, payload_len_hop)] = payload_len
     
-    testttl.print_hops(hops)
+    #print '-----'
+    #print 'Using:'    
+    using_hops = []
+    for host, hop in best_hops.keys():
+        real_mtu = testttl.confirm_max_mtu(host, hop)
+        if real_mtu > 64:
+            using_hops.append( (host, hop, real_mtu) )
+    return using_hops
+
+
+
+if __name__ == "__main__":
+    for host, hop, real_mtu in get_hops():
+        print '%s hop %d (MTU %d)' % (host, hop, real_mtu)
